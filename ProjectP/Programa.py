60c31f89bdb771c29bffe2c0cf27c9a7c31e1a42
clarkan-8 <110545003+clarkan-8@users.noreply.github.com>
2023-04-20 00:56:46 -0500

    Add files via upload

---
+++ ProjectP/Programa.py
@@ -0,0 +1,14 @@
+#cree un programa que al recibir como datos numericos enteros
+# calcule la suma resta y multiplicacion de dichos numeros
+
+
+
+
+n1=float(input("Ingrese el primer numero"))
+n2=float(input("Ingrese el segundo numero"))
+
+c= (n1+n2)
+resta= (n1-n2)
+multiplicacion= (n1*n2)
+print(c, resta, multiplicacion)
+
